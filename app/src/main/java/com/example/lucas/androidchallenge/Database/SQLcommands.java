eee43c772a185689f8c4eafcc64de09b675f4ae7
lucas <lucasbrandaodossantos@outlook.com>
2018-08-15 16:31:27 -0300

    Create the layout screens and the classes

--- app/src/main/java/com/example/lucas/androidchallenge/Database/SQLcommands.java
+++ app/src/main/java/com/example/lucas/androidchallenge/Database/SQLcommands.java
@@ -13,43 +13,4 @@ public class SQLcommands {
         return createtable.toString();
     }
 
-    public static String getInsertInto(){
-
-        StringBuilder insertinto = new StringBuilder();
-
-        insertinto.append ("INSERT INTO PERSON(NAME, AGE)");
-        insertinto.append ("VALUES");
-
-        return insertinto.toString();
-    }
-
-    public static String getUpdate(){
-        StringBuilder update = new StringBuilder();
-
-        update.append ("UPDATE PERSON(");
-        update.append ("SET NAME");
-        update.append ("        AGE  INTEGER (2) NOT NULL);");
-
-        return update.toString();
-    }
-
-    public static String getDelete(){
-        StringBuilder delete = new StringBuilder();
-
-        delete.append ("CREATE TABLE IF NOT EXISTS PERSON(");
-        delete.append ("        NAME VARCHAR (250) NOT NULL,");
-        delete.append ("        AGE  INTEGER (2) NOT NULL);");
-
-        return delete.toString();
-    }
-
-    public static String getSelect(){
-        StringBuilder select = new StringBuilder();
-
-        select.append ("CREATE TABLE IF NOT EXISTS PERSON(");
-        select.append ("        NAME VARCHAR (250) NOT NULL,");
-        select.append ("        AGE  INTEGER (2) NOT NULL);");
-
-        return select.toString();
-    }
 }
